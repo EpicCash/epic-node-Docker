45a3e65508dfd21bc950c57c51bdce6bf7b2a33c
Sky King <skyking@epiccash.com>
2025-12-04 19:08:56 -0500

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+if [ -e ~/.epic/main/bs ]
+then
+	echo "chain_data exists - no bootstrap needed..."
+else
+	echo "Setting up bootstrap chain_data file..."
+	wget -nv --show-progress https://bootstrap.epiccash.com/bootstrap.zip -P ~/.epic/main
+	rm -R ~/.epic/main/chain_data
+	unzip ~/.epic/main/bootstrap.zip -d ~/.epic/main
+	rm ~/.epic/main/bootstrap.zip
+	touch ~/.epic/main/bs
+	echo "Done"
+fi
 
-/bin/screen -dmS node-server /home/epicnode/epic-node
+/usr/bin/screen -dmS node-server /home/epicnode/epic-node
 tail -f /dev/null
 
